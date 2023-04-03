b6b34b848d1b524faf4b83296e15c254498d0f1a
goyounha11 <goyounha11@naver.com>
2023-04-03 13:39:59 +0900

    독립 실행형 서블릿 애플리케이션 - 서블릿 등록

--- src/main/java/com/toby/helloboot/HellobootApplication.java
+++ src/main/java/com/toby/helloboot/HellobootApplication.java
@@ -5,13 +5,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
         // 빈서블렛 띄우기
         TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = tomcatServletWebServerFactory.getWebServer();
+        WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type", "text/plain");
+                    resp.getWriter().println("Hello Servlet");
+                }
+            }).addMapping("/hello");
+        });
         webServer.start();
     }
 
