1e2248aca310471d177f5acc165b60a0d4489919
goyounha11 <goyounha11@naver.com>
2023-04-04 15:02:36 +0900

    독립 실행형 스프링 애플리케이션 - 의존 오브젝트 추가

--- src/main/java/com/toby/helloboot/HelloController.java
+++ src/main/java/com/toby/helloboot/HelloController.java
@@ -3,8 +3,14 @@ package com.toby.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+
 public class HelloController {
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        return helloService.sayHello(requireNonNull(name));
     }
 }
---
+++ src/main/java/com/toby/helloboot/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package com.toby.helloboot;
+
+public class SimpleHelloService {
+    String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
